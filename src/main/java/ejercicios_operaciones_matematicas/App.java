f990483c1390d8534fd1efc2329db131bd79d610
AlejandroJ1 <alejandro.jimenezmartin@tiernoparla.es>
2023-04-23 13:05:43 +0200

    ejercicios repaso condicionales

--- src/main/java/ejercicios_operaciones_matematicas/App.java
+++ src/main/java/ejercicios_operaciones_matematicas/App.java
@@ -42,12 +42,14 @@ public class App {
          * double total = baseImponible * iva;
          * System.out.println("El precio total es: " + total);
          */
+
         /*
          * char letra = 'A';
          * String cadena = "ABC"
          * 
          * System.out.println("Juntamos el char" + letra + "con el String" + cadena);
          */
+
         /*
          * //Concatenar varios char en una String
          * char letra1 = 'A';
@@ -60,6 +62,7 @@ public class App {
          * letra5;
          * System.out.println(cadena);
          */
+
         /*
          * //Ejercicio con Scanner meter dos numeros y que de la muliplicacion de los
          * numeros
@@ -77,6 +80,7 @@ public class App {
          * System.out.println("El resultado de la multiplicacion de " + numero1 + " y "
          * + numero2 + " es: " + multiplicacion);
          */
+
         /*
          * //Introducir dos numeros con scanner y hacer todas las operaciones con eso
          * numeros
@@ -96,6 +100,7 @@ public class App {
          * System.out.println("Multiplicacion: " + multiplicacion);
          * System.out.println("Division: " + division);
          */
+
         /*
          * //Calcular el area de un triangulo
          * Scanner sc = new Scanner(System.in);
@@ -112,6 +117,7 @@ public class App {
          * System.out.println("El area del tringulo con base " + base + " y altura " +
          * altura + " es " + area);
          */
+
         /*
          * //Calcular lo que cobra un trabajador en una semana
          * Scanner sc = new Scanner(System.in);
@@ -125,6 +131,7 @@ public class App {
          * System.out.println("Horas trabajadas: " + horas_trabajadas);
          * System.out.println("El salario semanal es: " + salario_semanal);
          */
+
         /*
          * //Calcular la nota que hay que sacar en en examen2 para la nota deseada
          * Scanner sc = new Scanner(System.in);
@@ -140,6 +147,101 @@ public class App {
          * 
          * System.out.println("Para obtener una media de " + mediaDeseada +
          * " necesitas sacar un " + nota2 + " en el segundo examen.");
+         */
+
+        /*
+         * //Poner numero y te dice día de la semana
+         * 
+         * Scanner sc = new Scanner(System.in);
+         * int numero_semana;
+         * String nombre_semana = "";
+         * 
+         * System.out.println("Dime un día de la semana(numericamente)");
+         * numero_semana = sc.nextInt();
+         * 
+         * switch(numero_semana){
+         * case 1:
+         * nombre_semana = "Lunes";
+         * break;
+         * case 2:
+         * nombre_semana = "Martes";
+         * break;
+         * case 3:
+         * nombre_semana = "Miercoles";
+         * break;
+         * case 4:
+         * nombre_semana = "Jueves";
+         * break;
+         * case 5:
+         * nombre_semana = "Viernes";
+         * break;
+         * case 6:
+         * nombre_semana = "Sabado";
+         * break;
+         * case 7:
+         * nombre_semana = "Domingo";
+         * break;
+         * 
+         * default:
+         * System.out.
+         * println("No existe un día de la semana relacionado con ese numero.");
+         * 
+         * }
+         * System.out.println("El numero " + numero_semana + " es igual al " +
+         * nombre_semana);
+         */
+
+        /*
+         * //calcular lo que se cobra con 40 y más de 40 horas
+         * final int SUELDO = 12;
+         * final int SUELDO_MAYOR_CUARENTA = 16;
+         * Scanner sc = new Scanner(System.in);
+         * int horas;
+         * int sueldo_total;
+         * System.out.println("¿Cuantas horas a trabajado en la semana?:");
+         * horas = sc.nextInt();
+         * 
+         * if(horas > 40){
+         * sueldo_total = (40 * SUELDO) + (horas - 40) * SUELDO_MAYOR_CUARENTA;
+         * }else{
+         * sueldo_total = horas * SUELDO;
+         * }
+         * 
+         * System.out.println("El sueldo semanal es " + sueldo_total);
+         */
+
+        /*
+         * //Ecuacion de primer grado(ax+b=0)
+         * Scanner sc = new Scanner(System.in);
+         * double a;
+         * System.out.println("Dime el valor de a:");
+         * a = sc.nextDouble();
+         * double b;
+         * System.out.println("Dime el valor de b:");
+         * b = sc.nextDouble();
+         * double x;
+         * if (a == 0) {
+         * System.out.println("Ecuacion indeterminada(no podemos dividir b entre 0)");
+         * } else if (b == 0) {
+         * System.out.println("Ecuacion indeterminada(no podemos divir 0 entre a)");
+         * } else {
+         * x = -b / a;
+         * System.out.println("La solucion es:" + x);
+         * }
+         */
+
+        /*
+         * //aplicar formula a t =√2h/g siendo g = 9.81m/s2
+         * final double GRAVEDAD = 9.81;
+         * Scanner sc = new Scanner(System.in);
+         * double altura;
+         * System.out.println("Dime la altura para aplicar la formula:");
+         * altura = sc.nextDouble();
+         * double tiempo;
+         * 
+         * tiempo = Math.sqrt(2*altura/GRAVEDAD);
+         * 
+         * System.out.println("El tiempo que tarda es:" + tiempo);
          */
     }
 }
